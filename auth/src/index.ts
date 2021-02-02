63af0bf4554e7b373765c87562cff347afd66d37
Wittman Arley Gutiérrez Torres <wa.gutierrez@soprasteria.com>
2021-02-02 17:19:01 +0100

    140. Add MongoDB deployment config file. Add connection to mongo with mongoose.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -26,6 +28,22 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('AUTH: Listening on port 3000');
-});
+const start = async () => {
+  try {
+    //mongodb://<url-mongodb>:<port>/<database-name>
+    // if the database doesn't exist, it will be create id
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('AUTH: Listening on port 3000');
+  });
+};
+
+start();
